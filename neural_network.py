c866add70e7f7e2d5c7f2a6163a550cc5e82c6b4
dalto135 <67397294+dalto135@users.noreply.github.com>
2024-01-23 22:44:34 -0500

    initial commit, neural network is working

---
+++ neural_network.py
@@ -0,0 +1,183 @@
+import numpy as np
+import pandas as pd
+from matplotlib import pyplot as plt
+
+RED = '\033[91m'
+YELLOW = '\033[93m'
+GREEN = '\033[92m'
+BLUE = '\033[96m'
+PURPLE = '\033[94m'
+RESET = '\033[0m'
+
+data = pd.read_csv('train.csv')
+
+# m is number of training examples (42,000)
+# n is the number of pixels per example, plus 1 for the label column (28 x 28 + 1 = 785 in total)
+data = np.array(data)
+m, n = data.shape
+np.random.shuffle(data) # shuffle before splitting into dev and training sets
+
+# data_dev is the transpose of the first 1000 training examples, which will be used for testing the trained network
+# Y_dev is the first row of data_dev, or an array of each of the ground-truth values of the first 1000 training examples
+# X_dev is the remaining 784 rows of data_dev, each row representing the next pixle for each of the first 1000 training examples
+data_dev = data[0:1000].T
+Y_dev = data_dev[0]
+X_dev = data_dev[1:n]
+X_dev = X_dev / 255.
+
+# data_train is the transpose of the remaining 41,000 training examples, which will be used to train the network
+# Y_train is the first row of data_train, or an array of each of the ground-truth values of the remaining 41,000 training examples
+# X_train is the remaining 784 rows of data_train, each row representing the next pixle for each of the remaining 41,000 training examples
+data_train = data[1000:m].T
+Y_train = data_train[0]
+X_train = data_train[1:n]
+X_train = X_train / 255.
+_,m_train = X_train.shape
+
+# np.random.rand(x, y) gives x arrays, y elements per array, each element a random decimal between 0 and 1
+# Ex. W1 and b1 correspond to layer 1, the hidden layer,
+# which has 10 nodes, each recieving input from each of the 784 nodes of the input layer
+def init_params():
+    W1 = np.random.rand(10, 784) - 0.5
+    b1 = np.random.rand(10, 1) - 0.5
+    W2 = np.random.rand(10, 10) - 0.5
+    b2 = np.random.rand(10, 1) - 0.5
+
+    return W1, b1, W2, b2
+
+def ReLU(Z):
+    return np.maximum(Z, 0)
+
+# np.exp(Z) is e^Z
+# This function takes an array and converts each element into (e^element / the sum of e^each element)
+def softmax(Z):
+    return np.exp(Z) / sum(np.exp(Z))
+
+# X is an array of the 784 input nodes(?)
+def forward_prop(W1, b1, W2, b2, X):
+    Z1 = W1.dot(X) + b1
+    A1 = ReLU(Z1)
+    Z2 = W2.dot(A1) + b2
+    A2 = softmax(Z2)
+
+    return Z1, A1, Z2, A2
+
+# For RelU, Y = 0 when X <= 0, and Y = X when X > 0
+# Taking the derivative of this graph gives 0 when X <= 0 and 1 when X > 0
+def ReLU_deriv(Z):
+    return Z > 0
+
+# For each element in Y, this function converts that element into an array of all zeroes,
+# except the element corresponding to the ground-truth number, which is set to 1
+def one_hot(Y):
+    one_hot_Y = np.zeros((Y.size, Y.max() + 1))
+    one_hot_Y[np.arange(Y.size), Y] = 1
+    one_hot_Y = one_hot_Y.T
+
+    return one_hot_Y
+
+# Calculates the loss values for each of the weights and biases(?)
+# Y is the ground-truth value
+def backward_prop(Z1, A1, Z2, A2, W1, W2, X, Y):
+    one_hot_Y = one_hot(Y)
+
+    dZ2 = A2 - one_hot_Y
+    dW2 = 1 / m * dZ2.dot(A1.T)
+    db2 = 1 / m * np.sum(dZ2)
+
+    dZ1 = W2.T.dot(dZ2) * ReLU_deriv(Z1)
+    dW1 = 1 / m * dZ1.dot(X.T)
+    db1 = 1 / m * np.sum(dZ1)
+
+    return dW1, db1, dW2, db2
+
+def update_params(W1, b1, W2, b2, dW1, db1, dW2, db2, alpha):
+    W1 = W1 - alpha * dW1
+    b1 = b1 - alpha * db1
+    W2 = W2 - alpha * dW2
+    b2 = b2 - alpha * db2
+
+    return W1, b1, W2, b2
+
+def get_predictions(A2):
+    return np.argmax(A2, 0)
+
+def get_accuracy(predictions, Y):
+    return np.sum(predictions == Y) / Y.size
+
+def gradient_descent(X, Y, alpha, iterations):
+    W1, b1, W2, b2 = init_params()
+
+    for i in range(iterations):
+        Z1, A1, Z2, A2 = forward_prop(W1, b1, W2, b2, X)
+        dW1, db1, dW2, db2 = backward_prop(Z1, A1, Z2, A2, W1, W2, X, Y)
+        W1, b1, W2, b2 = update_params(W1, b1, W2, b2, dW1, db1, dW2, db2, alpha)
+
+        if i % 10 == 0:
+            print()
+            print("Iteration:", i)
+            predictions = get_predictions(A2)
+            if get_accuracy(predictions, Y) < 0.5:
+                print("Accuracy: " + RED + str(get_accuracy(predictions, Y)) + RESET)
+            elif get_accuracy(predictions, Y) < 0.8:
+                print("Accuracy: " + YELLOW + str(get_accuracy(predictions, Y)) + RESET)
+            elif get_accuracy(predictions, Y) < 0.9:
+                print("Accuracy: " + GREEN + str(get_accuracy(predictions, Y)) + RESET)
+            elif get_accuracy(predictions, Y) < 0.95:
+                print("Accuracy: " + BLUE + str(get_accuracy(predictions, Y)) + RESET)
+            else:
+                print("Accuracy: " + PURPLE + str(get_accuracy(predictions, Y)) + RESET)
+
+    return W1, b1, W2, b2
+
+num_of_samples = 500
+# num_of_samples = data_train[0].size
+W1, b1, W2, b2 = gradient_descent(X_train, Y_train, 0.10, num_of_samples)
+
+def make_predictions(X, W1, b1, W2, b2):
+    _, _, _, A2 = forward_prop(W1, b1, W2, b2, X)
+    predictions = get_predictions(A2)
+
+    return predictions
+
+def test_prediction(index, W1, b1, W2, b2):
+    current_image = X_train[:, index, None]
+    prediction = make_predictions(X_train[:, index, None], W1, b1, W2, b2)
+    label = Y_train[index]
+    print("Prediction:", prediction)
+    print("Label:", label)
+    
+    current_image = current_image.reshape((28, 28)) * 255
+    plt.gray()
+    plt.imshow(current_image, interpolation='nearest')
+    plt.show()
+
+# test_prediction(0, W1, b1, W2, b2)
+# test_prediction(1, W1, b1, W2, b2)
+# test_prediction(2, W1, b1, W2, b2)
+# test_prediction(3, W1, b1, W2, b2)
+
+dev_predictions = make_predictions(X_dev, W1, b1, W2, b2)
+
+score_array = []
+for i in range(dev_predictions.size):
+    if dev_predictions[i] == Y_dev[i]:
+        score_array.append(dev_predictions[i])
+    else:
+        score_array.append(str(dev_predictions[i]) + "," + str(Y_dev[i]))
+
+print()
+print("Test Set:")
+print('[' + ', '.join(GREEN + str(score_array[i]) + RESET if score_array[i] == Y_dev[i] else RED + str(score_array[i]) + RESET for i in range(len(score_array))) + ']')
+
+print()
+if get_accuracy(dev_predictions, Y_dev) < 0.5:
+    print("Test Set Accuracy: " + RED + str(get_accuracy(dev_predictions, Y_dev)) + RESET)
+elif get_accuracy(dev_predictions, Y_dev) < 0.8:
+    print("Test Set Accuracy: " + YELLOW + str(get_accuracy(dev_predictions, Y_dev)) + RESET)
+elif get_accuracy(dev_predictions, Y_dev) < 0.9:
+    print("Test Set Accuracy: " + GREEN + str(get_accuracy(dev_predictions, Y_dev)) + RESET)
+elif get_accuracy(dev_predictions, Y_dev) < 0.95:
+    print("Test Set Accuracy: " + BLUE + str(get_accuracy(dev_predictions, Y_dev)) + RESET)
+else:
+    print("Test Set Accuracy: " + PURPLE + str(get_accuracy(dev_predictions, Y_dev)) + RESET)
---
+++ yee.py
@@ -0,0 +1,24 @@
+import numpy as np
+
+W2 = np.random.rand(10, 5)
+print(W2)
+print(W2.size)
+print(len(W2))
+print(len(W2[0]))
+print(W2[0].size)
+
+array = [
+    [5, 5, 5, 5, 5],
+    [5, 5, 5, 5, 5],
+    [5, 5, 5, 5, 5],
+    [5, 5, 5, 5, 5],
+    [5, 5, 5, 5, 5],
+    [5, 5, 5, 5, 5],
+    [5, 5, 5, 5, 5],
+    [5, 5, 5, 5, 5],
+    [5, 5, 5, 5, 5],
+    [5, 5, 5, 5, 5]
+]
+
+print(len(array))
+print(len(array[0]))
